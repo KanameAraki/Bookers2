c82dc893113ad9d34ad16c4a9ebe678c478eb333
KanameAraki <kana911me@gmail.com>
2021-04-02 22:27:44 +0000

    [Finish]favorite,comment

--- Gemfile
+++ Gemfile
@@ -30,8 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+# o for deployment
+# gem 'capistraUse Capistranno-rails', group: :development
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "home/about"=>"home#about"
 
   resources:users,only:[:show,:index,:edit,:update]
-  resources:books,only:[:create,:index,:show,:destroy,:edit,:update]
-  resources:favorites,only:[:create,:destroy]
+  resources:books,only:[:create,:index,:show,:destroy,:edit,:update] do
+    resource:favorites,only:[:create,:destroy]
+    resources:book_comments,only:[:create,:destroy]
+  end
 end
