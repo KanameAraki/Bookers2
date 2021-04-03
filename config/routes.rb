4db2e92e0d38c88f3c46a3cbb38668db940a5141
KanameAraki <kana911me@gmail.com>
2021-04-03 05:51:32 +0000

    [Finish]follow

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,18 @@ Rails.application.routes.draw do
   root to: "home#top"
   get "home/about"=>"home#about"
 
-  resources:users,only:[:show,:index,:edit,:update]
+  resources:users,only:[:show,:index,:edit,:update] do
+    member do
+      get :followeds, :followers
+    end
+    resources:relationships,only:[:create,:destroy]
+  end
+
+# resources:relationships,only:[:create,:destroy]
+
   resources:books,only:[:create,:index,:show,:destroy,:edit,:update] do
     resource:favorites,only:[:create,:destroy]
     resources:book_comments,only:[:create,:destroy]
   end
+
 end
